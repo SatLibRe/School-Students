dc543ca6576c08163c0ba7b20e08778041fc3c80
Miles Higbie <miles.higbie@gmail.com>
2020-02-03 10:37:24 -0500

    first_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/schools', to: "schools#index"
+  resources :schools
+  resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-School.create!(name: "Flatiron School", address:"11 Broadway")
-School.create!(name: "NYU", address:"New York, NY 10003")
-School.create!(name: "John Jay", address:"524 W 59th St, New York, NY 10019")
-School.create!(name: "City College", address:"160 Convent Ave, New York, NY 10031")
-School.create!(name: "Columbia University", address:"116th St & Broadway, New York, NY 10027")
+Student.destroy_all
+School.destroy_all
+
+s1 = School.create!(name: "Flatiron School", address:"11 Broadway")
+s2 = School.create!(name: "NYU", address:"New York, NY 10003")
+s3 = School.create!(name: "John Jay", address:"524 W 59th St, New York, NY 10019")
+s4 = School.create!(name: "City College", address:"160 Convent Ave, New York, NY 10031")
+s5 = School.create!(name: "Columbia University", address:"116th St & Broadway, New York, NY 10027")
+
+stu1 = Student.create(name: "Miles", age: "26")
+stu2 = Student.create(name: "Adam", age: "40")
+stu3 = Student.create(name: "Eddy", age: "50")
+stu4 = Student.create(name: "Alex", age: "10")
